0428f5dbeb3d01fb037fbe7a7fe34b2578046e57
Michael Krog <mic@apaq.dk>
2022-07-07 09:17:43 +0200

    Moves logic to seperate mjs file and adds simple time distance logic.

---
+++ client/controller.mjs
@@ -0,0 +1,97 @@
+const MINUTE_MILLIS = 60000;
+const HOUR_MILLIS = MINUTE_MILLIS * 60;
+const DAY_MILLIS = HOUR_MILLIS * 24;
+const WEEK_MILLIS = DAY_MILLIS * 7;
+const FOUR_WEEK_MILLIS = WEEK_MILLIS * 4;
+
+function mapDate(message) {
+  message.timestamp = new Date(message.timestamp);
+  return message;
+}
+
+function fetchMessages() {
+  return fetch('/api/messages')
+    .then(response => response.json())
+    .then(messages => messages.map(m => mapDate(m)));
+}
+
+function postMessage(message) {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(message),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return fetch('/api/messages', options)
+    .then(response => response.json())
+    .then(message => mapDate(message));
+}
+
+function interpolate(template, params) {
+  const replaceTags = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '(': '&#40;', ')': '&#41;' };
+  const safeInnerHTML = text => text.toString().replace(/[&<>\(\)]/g, tag => replaceTags[tag] || tag);
+  const keys = Object.keys(params);
+  const keyVals = Object.values(params).map(safeInnerHTML);
+  return new Function(...keys, `return \`${template}\``)(...keyVals);
+}
+
+function formatTimeDistance(date) {
+  const now = new Date();
+  const millisDistance = now.getTime() - date.getTime();
+
+  if (millisDistance < MINUTE_MILLIS) {
+    return 'now';
+  }
+
+  if (millisDistance < HOUR_MILLIS) {
+    return `${Math.round(millisDistance / MINUTE_MILLIS)} minutes ago`;
+  }
+
+  if (millisDistance < DAY_MILLIS) {
+    return `${Math.round(millisDistance / HOUR_MILLIS)} hours ago`;
+  }
+
+  if (millisDistance < WEEK_MILLIS) {
+    return `${Math.round(millisDistance / DAY_MILLIS)} days ago`;
+  }
+
+  return `${Math.round(millisDistance / WEEK_MILLIS)} weeks ago`;
+}
+
+function loadMessages() {
+  const template = document.getElementById('comment-entry-template');
+  const commentsContainer = document.getElementById('comments');
+
+  let html = '';
+  fetchMessages().then(messages => {
+    messages
+      .map(m => {
+        m.timeDistance = formatTimeDistance(m.timestamp);
+        return m;
+      })
+      .forEach(m => html += interpolate(template.innerHTML.toString().trim(), m));
+    commentsContainer.innerHTML = html;
+  });
+}
+
+const input = document.getElementById('comment-input');
+const submit = document.getElementById('comment-submit');
+
+// Bind event listeners
+submit.addEventListener('click', () => {
+  const message = {
+    username: 'rob',
+    name: 'Rob Hope',
+    text: document.getElementById('comment-input').value
+  };
+
+  postMessage(message).then(() => {
+    input.value = '';
+    loadMessages();
+  });
+});
+
+// Do initial comment fetching
+loadMessages();
