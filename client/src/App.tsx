25e4b95ec997fe0a686a047a45b4d5b3a9ef38f8
Michael Krog <mic@apaq.dk>
2022-07-08 13:14:27 +0200

    Adds initial fetch of messages from backend.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Avatar from './components/avatar/Avatar';
 import Comment from './components/comment/Comment';
 import { Message } from './models/message';
 
-function App() {
+class App extends Component<{}, { messages: Message[] }> {
 
-  const message: Message = {
-    createdDate: new Date(),
-    lastModifiedDate: new Date(),
-    name: 'Rob Hope',
-    username: 'rob',
-    text: 'Qwerty 12345',
-    votes: 0
-  };
+  state = { messages: [] };
 
-  return (
-    
-    <div className="App">
-      <h1>Discussion</h1>
+  componentDidMount() {
+    fetch('/api/messages', { headers: { accept: 'application/json' }, cache: 'no-cache' })
+      .then(response => response.json())
+      .then(messages => this.setState({ messages }));
+  }
+
+  render() {
+    return (
+
+      <div className="App">
+        <h1>Discussion</h1>
         <div className="comment-input">
-            <Avatar username='rob'></Avatar>
-            <input className="comment-input__input" placeholder="What are your thoughts?" type="text"></input>
-            <button className="comment-input__button">Comment</button>
+          <Avatar username='rob'></Avatar>
+          <input className="comment-input__input" placeholder="What are your thoughts?" type="text"></input>
+          <button className="comment-input__button">Comment</button>
         </div>
 
         <hr></hr>
 
         <div className="comments">
-          <Comment message={message}></Comment>
+          {this.state.messages.map((message: Message) => <Comment key={message.id} message={message}></Comment>)}
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
