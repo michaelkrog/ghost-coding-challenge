625d2dbc8193e4779e6551fea18cf0c36c7f0d9e
Michael Krog <mic@apaq.dk>
2022-07-09 08:47:16 +0200

    Readds support for static serving of files.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { MessageController } from './controllers/message.controller';
 import { MessageService } from './services/message.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [MessageController],
   providers: [MessageService],
 })
